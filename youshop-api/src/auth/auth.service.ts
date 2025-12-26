794671d574c6e0685b507d57f9573b259455d083
ghandour <abdelhakghandour6@gmail.com>
2025-12-26 16:20:57 +0100

    YOUC-9

---
+++ youshop-api/src/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(email: string, password: string, name?: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      },
+    });
+
+    const { password: _, ...result } = user;
+    return {
+      user: result,
+      token: this.jwtService.sign({ sub: user.id, email: user.email }),
+    };
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const { password: _, ...result } = user;
+    return {
+      user: result,
+      token: this.jwtService.sign({ sub: user.id, email: user.email }),
+    };
+  }
+}
